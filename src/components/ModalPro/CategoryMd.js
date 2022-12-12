0daf6a335f8ea445bc5300aecb9aa4e8336a267f
Tran An Thuyen <thuyen8tui@gmail.com>
2022-12-12 19:10:35 +0700

    thuyen categories

--- src/components/ModalPro/CategoryMd.js
+++ src/components/ModalPro/CategoryMd.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAdd } from "@fortawesome/free-solid-svg-icons";
 import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
+import "./style.css";
 
 function CategoryMd() {
   const [show, setShow] = useState(false);
@@ -11,30 +14,19 @@ function CategoryMd() {
 
   return (
     <>
-      <Button variant="primary" onClick={handleShow}>
-        Add Category
+      <Button className="btn-newct" variant="primary" onClick={handleShow}>
+        <FontAwesomeIcon icon={faAdd} /> New Category
       </Button>
 
       <Modal show={show} onHide={handleClose}>
-        <Modal.Header  closeButton>
-          <Modal.Title>Category Edit</Modal.Title>
+        <Modal.Header closeButton>
+          <Modal.Title>Add New Categories</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="name@example.com"
-                autoFocus
-              />
-            </Form.Group>
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-            >
-              <Form.Label>Example textarea</Form.Label>
-              <Form.Control as="textarea" rows={3} />
+              <Form.Label>Category Name</Form.Label>
+              <Form.Control type="email" placeholder="" autoFocus />
             </Form.Group>
           </Form>
         </Modal.Body>
@@ -43,7 +35,7 @@ function CategoryMd() {
             Close
           </Button>
           <Button variant="primary" onClick={handleClose}>
-            Save Changes
+            Add
           </Button>
         </Modal.Footer>
       </Modal>
--- src/pages/Categories/index.jsx
+++ src/pages/Categories/index.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
 // Table from react-bootstrap
 import { Table } from "react-bootstrap";
+import { calculateRange, sliceData } from "../../utils/table-pagination";
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faRemove } from "@fortawesome/free-solid-svg-icons";
 // To make rows collapsible
 import CategoryMd from "../../components/ModalPro/CategoryMd";
 import { useEffect, useState } from "react";
@@ -16,45 +19,93 @@ function Categories() {
   const callAPI = async (callback) => {
     const res = await getAllCategory();
     callback(res.data.data);
-  }
-
+  };
+  const [orders, setOrders] = useState(data);
+  const [page, setPage] = useState(1);
+  const [pagination, setPagination] = useState([]);
+  useEffect(() => {
+    setPagination(calculateRange(data, 5));
+    setOrders(sliceData(data, page, 5));
+    console.log(data.length);
+  }, []);
   useEffect(() => {
     callAPI(setData);
   }, []);
+  const __handleChangePage = (new_page) => {
+    setPage(new_page);
+    setOrders(sliceData(data, new_page, 5));
+  };
 
   return (
     <>
       <div className="dashboard-content">
-        <DashboardHeader btnText="New Order" />
+        <DashboardHeader />
 
         <div className="dashboard-content-container">
           <div className="dashboard-content-header">
-            <h2>Orders List</h2>
+            <h2>Category List</h2>
           </div>
-          <CategoryMd />
-          <table className="list-promotion">
+          <div>
+            <CategoryMd />
+          </div>
+          <table className="list-category-table">
             <thead>
               <tr>
                 <th>STT</th>
-                <th> Name</th>
-                <th>Created at</th>
+                <th>CATEGORY NAME</th>
+                <th>CREATED AT</th>
+                <th>UPDATE AT</th>
+                <th>ACTION</th>
               </tr>
             </thead>
             <tbody>
               {data.map((row, index) => (
                 <tr key={index}>
                   <td>
-                    <span>{row.id}</span>
+                    <span className="rowct">{row.id}</span>
                   </td>
-                  <span>{row.name}</span>
-
-                  <span>{row.createdAt}</span>
-
-                  <td></td>
+                  <td>
+                    <span className="rowct">{row.name}</span>
+                  </td>
+                  <td>
+                    <span className="rowct">{row.createdAt}</span>
+                  </td>
+                  <td>
+                    <span className="rowct">{row.updatedAt}</span>
+                  </td>
+                  <td>
+                    <button className="rowedit">
+                      {" "}
+                      <FontAwesomeIcon icon={faEdit} />
+                      &nbsp; Edit &nbsp;&nbsp;
+                    </button>
+                    <button className="rowremove">
+                      {" "}
+                      <FontAwesomeIcon icon={faRemove} />
+                      &nbsp; Delete
+                    </button>
+                  </td>
                 </tr>
               ))}
             </tbody>
           </table>
+          {data.length !== 0 ? (
+            <div className="dashboard-content-footer">
+              {pagination.map((item, index) => (
+                <span
+                  key={index}
+                  className={item === page ? "active-pagination" : "pagination"}
+                  onClick={() => __handleChangePage(item)}
+                >
+                  {item}
+                </span>
+              ))}
+            </div>
+          ) : (
+            <div className="dashboard-content-footer">
+              <span className="empty-table">No data</span>
+            </div>
+          )}
         </div>
       </div>
     </>
